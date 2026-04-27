24852ae16aa36948f99a1bf3cf536380c8b5f5c2
KGFCH2 <babinbid05@gmail.com>
2026-04-28 01:40:43 +0530

    Implemented layout animations using motion div for better user experience

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 
 export function Layout() {
+  const location = useLocation();
+  
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
       <main className="flex-1">
-        <Outlet />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </main>
       <Footer />
     </div>
